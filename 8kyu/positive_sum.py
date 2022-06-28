7d4086701bc3077745b8008853319d0572f3d951
atinyame <104745335+atinyame@users.noreply.github.com>
2022-06-27 23:17:22 -0500

    8kyu: Sum of positive

---
+++ 8kyu/positive_sum.py
@@ -0,0 +1,22 @@
+import codewars_test as test
+
+
+def positive_sum(arr: list) -> int:
+    return sum(x for x in arr if x > 0)
+
+
+@test.describe("positive_sum")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(positive_sum([1, 2, 3, 4, 5]), 15)
+        test.assert_equals(positive_sum([1, -2, 3, 4, 5]), 13)
+        test.assert_equals(positive_sum([-1, 2, 3, 4, -5]), 9)
+
+    @test.it("returns 0 when array is empty")
+    def empty_case():
+        test.assert_equals(positive_sum([]), 0)
+
+    @test.it("returns 0 when all elements are negative")
+    def negative_case():
+        test.assert_equals(positive_sum([-1, -2, -3, -4, -5]), 0)
