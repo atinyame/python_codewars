30481a3eac57b9ee0c83f55376f2fa78b8131be1
atinyame <104745335+atinyame@users.noreply.github.com>
2022-06-28 15:42:31 -0500

    8kyu: Convert a String to a Number!

---
+++ 8kyu/string_to_number.py
@@ -0,0 +1,15 @@
+import codewars_test as test
+
+
+def string_to_number(s: str) -> int:
+    return int(s)
+
+
+@test.describe("string_to_number")
+def basic_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(string_to_number("1234"), 1234)
+        test.assert_equals(string_to_number("605"), 605)
+        test.assert_equals(string_to_number("1405"), 1405)
+        test.assert_equals(string_to_number("-7"), -7)
