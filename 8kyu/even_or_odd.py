08232d34f14f00f5f11fa92edf8c6532c7b2a5a2
atinyame <104745335+atinyame@users.noreply.github.com>
2022-06-27 23:07:46 -0500

    8kyu: Even or Odd

---
+++ 8kyu/even_or_odd.py
@@ -0,0 +1,22 @@
+import codewars_test as test
+
+
+def even_or_odd(number: int) -> str:
+    return "Even" if number % 2 == 0 else "Odd"
+
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(even_or_odd(2), "Even")
+        test.assert_equals(even_or_odd(1), "Odd")
+        test.assert_equals(even_or_odd(0), "Even")
+        test.assert_equals(even_or_odd(1545452), "Even")
+        test.assert_equals(even_or_odd(7), "Odd")
+        test.assert_equals(even_or_odd(78), "Even")
+        test.assert_equals(even_or_odd(17), "Odd")
+        test.assert_equals(even_or_odd(74156741), "Odd")
+        test.assert_equals(even_or_odd(100000), "Even")
+        test.assert_equals(even_or_odd(-123), "Odd")
+        test.assert_equals(even_or_odd(-456), "Even")
