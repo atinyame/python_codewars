d1a51a85ca4c7e8ddcbb33c0ff1db7511f269a9d
atinyame <104745335+atinyame@users.noreply.github.com>
2022-06-27 23:08:09 -0500

    8kyu: Opposite number

---
+++ 8kyu/opposite.py
@@ -0,0 +1,17 @@
+import codewars_test as test
+
+
+def opposite(number: float) -> float:
+    return -number
+
+
+@test.describe("Fixed Tests")
+def basic_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(opposite(1), -1)
+        test.assert_equals(opposite(25.6), -25.6)
+        test.assert_equals(opposite(0), 0)
+        test.assert_equals(opposite(1425.2222), -1425.2222)
+        test.assert_equals(opposite(-3.1458), 3.1458)
+        test.assert_equals(opposite(-95858588225), 95858588225)
