72b648c128744bdb1001d25eb2e93f368833d59d
atinyame <104745335+atinyame@users.noreply.github.com>
2022-06-28 18:51:02 -0500

    8kyu: Abbreviate a Two Word Name

---
+++ 8kyu/abbrev_name.py
@@ -0,0 +1,16 @@
+import codewars_test as test
+
+
+def abbrev_name(name: str) -> str:
+    return '.'.join(x[0].upper() for x in name.split())
+
+
+@test.describe("Fixed Tests")
+def basic_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(abbrev_name("Sam Harris"), "S.H")
+        test.assert_equals(abbrev_name("patrick feenan"), "P.F")
+        test.assert_equals(abbrev_name("Evan C"), "E.C")
+        test.assert_equals(abbrev_name("P Favuzzi"), "P.F")
+        test.assert_equals(abbrev_name("David Mendieta"), "D.M")
