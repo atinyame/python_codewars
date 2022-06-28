2538e2507bea1958e72c0f5ae5400f549159b057
atinyame <104745335+atinyame@users.noreply.github.com>
2022-06-28 15:49:19 -0500

    8kyu: Century From Year

---
+++ 8kyu/century.py
@@ -0,0 +1,22 @@
+import codewars_test as test
+
+
+def century(year: int) -> int:
+    return ((year - 1) // 100) + 1
+
+# import math
+
+# def century(year):
+#     return math.ceil(year / 100)
+
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(century(1705), 18, 'Testing for year 1705')
+        test.assert_equals(century(1900), 19, 'Testing for year 1900')
+        test.assert_equals(century(1601), 17, 'Testing for year 1601')
+        test.assert_equals(century(2000), 20, 'Testing for year 2000')
+        test.assert_equals(century(356), 4, 'Testing for year 356')
+        test.assert_equals(century(89), 1, 'Testing for year 89')
