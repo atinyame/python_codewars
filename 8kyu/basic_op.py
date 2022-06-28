7cd0c0ad9d7aca76e9faf92ec81eaad82babbba3
atinyame <104745335+atinyame@users.noreply.github.com>
2022-06-28 15:59:38 -0500

    8kyu: Basic Mathematical Operations

---
+++ 8kyu/basic_op.py
@@ -0,0 +1,25 @@
+import codewars_test as test
+
+
+def basic_op(operator: str, value1: int, value2: int) -> int | float:
+    match operator:
+        case '+':
+            return value1 + value2
+        case '-':
+            return value1 - value2
+        case '*':
+            return value1 * value2
+        case '/':
+            return value1 / value2
+        case _:
+            raise Exception('Invalid operator')
+
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(basic_op('+', 4, 7), 11)
+        test.assert_equals(basic_op('-', 15, 18), -3)
+        test.assert_equals(basic_op('*', 5, 5), 25)
+        test.assert_equals(basic_op('/', 49, 7), 7)
