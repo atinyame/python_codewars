efa7d940bc66da670801e23f03b05caa787d8d41
atinyame <104745335+atinyame@users.noreply.github.com>
2022-06-29 21:17:04 -0500

    8kyu: Convert a string to an array

---
+++ 8kyu/string_to_array.py
@@ -0,0 +1,17 @@
+import codewars_test as test
+
+
+def string_to_array(s: str) -> str:
+    return s.split(' ')
+
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(string_to_array("Robin Singh"), ["Robin", "Singh"])
+        test.assert_equals(string_to_array("CodeWars"), ["CodeWars"])
+        test.assert_equals(string_to_array("I love arrays they are my favorite"), [
+                           "I", "love", "arrays", "they", "are", "my", "favorite"])
+        test.assert_equals(string_to_array("1 2 3"), ["1", "2", "3"])
+        test.assert_equals(string_to_array(""), [""])
