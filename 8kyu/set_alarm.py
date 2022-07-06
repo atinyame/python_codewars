f9de169e8ba4a74d078b365162edd66a71b79c50
atinyame <104745335+atinyame@users.noreply.github.com>
2022-07-05 21:21:30 -0500

    8kyu: L1: Set Alarm

---
+++ 8kyu/set_alarm.py
@@ -0,0 +1,20 @@
+import codewars_test as test
+
+
+def set_alarm(employed: bool, vacation: bool) -> bool:
+    return employed and not vacation
+
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(set_alarm(True, True), False,
+                           "Fails when input is True, True")
+        test.assert_equals(set_alarm(False, True), False,
+                           "Fails when input is False, True")
+        test.assert_equals(set_alarm(False, False), False,
+                           "Fails when input is False, False")
+        test.assert_equals(set_alarm(True, False), True,
+                           "Fails when input is True, False")
+
