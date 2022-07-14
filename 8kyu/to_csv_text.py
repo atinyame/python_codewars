67df0ed5b3d34e457e98a445ed39484ba6653402
atinyame <104745335+atinyame@users.noreply.github.com>
2022-07-14 13:33:49 -0500

    8kyu: CSV representation of array

---
+++ 8kyu/to_csv_text.py
@@ -0,0 +1,42 @@
+import codewars_test as test
+
+
+def to_csv_text(array: list[list[int]]) -> str:
+    csv = ''
+    for line in array:
+        csv += ','.join(str(x) for x in line) + '\n'
+    return csv.removesuffix('\n')
+
+
+# def toCsvText(array):
+#     return '\n'.join(','.join(str(n) for n in lst) for lst in array)
+
+
+@test.it("Basic tests")
+def _():
+    test.assert_equals(
+        to_csv_text([
+            [0, 1, 2, 3, 45],
+            [10, 11, 12, 13, 14],
+            [20, 21, 22, 23, 24],
+            [30, 31, 32, 33, 34]
+        ]),
+        "0,1,2,3,45\n10,11,12,13,14\n20,21,22,23,24\n30,31,32,33,34",
+    )
+
+    test.assert_equals(
+        to_csv_text([
+            [-25, 21, 2, -33, 48],
+            [30, 31, -32, 33, -34]
+        ]),
+        "-25,21,2,-33,48\n30,31,-32,33,-34",
+    )
+
+    test.assert_equals(
+        to_csv_text([
+            [5, 55, 5, 5, 55],
+            [6, 6, 66, 23, 24],
+            [666, 31, 66, 33, 7]
+        ]),
+        "5,55,5,5,55\n6,6,66,23,24\n666,31,66,33,7",
+    )
